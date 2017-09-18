2651aab1b990feebe6553dd987b001074c253f7d
Anthony Chu <antchu@microsoft.com>
2017-09-18 05:51:05 +0000

    Upload files on kudu deploy

--- deployment/upload-www-to-blob.ps1
+++ deployment/upload-www-to-blob.ps1
@@ -1,3 +1,7 @@
-Get-ChildItem
+$destinationUri = "https://$($Env:STORAGE_HOSTNAME)/www"
+$destinationKey = $Env:STORAGE_KEY
+.\deployment\AzCopy.exe /Source:.\test /Dest:$destinationUri /DestKey:$destinationKey
 
+$storageContext = New-AzureStorageContext -ConnectionString $Env:STORAGE_CONNECTION
+Set-AzureStorageContainerAcl -Container "www" -Permission Container -Context $storageContext
 
