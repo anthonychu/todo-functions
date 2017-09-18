088b1b7357e9ba1cbfc7b747561f9250b42b321a
Anthony Chu <antchu@microsoft.com>
2017-09-18 06:00:39 +0000

    Fix path to azcopy

--- deployment/upload-www-to-blob.ps1
+++ deployment/upload-www-to-blob.ps1
@@ -2,7 +2,7 @@ $ProgressPreference="SilentlyContinue"
 
 $destinationUri = "https://$($Env:STORAGE_HOSTNAME)/www"
 $destinationKey = $Env:STORAGE_KEY
-.\deployment\AzCopy.exe /Source:.\test /Dest:$destinationUri /DestKey:$destinationKey
+.\deployment\AzCopy\AzCopy.exe /Source:.\test /Dest:$destinationUri /DestKey:$destinationKey
 
 $storageContext = New-AzureStorageContext -ConnectionString $Env:STORAGE_CONNECTION
 Set-AzureStorageContainerAcl -Container "www" -Permission Container -Context $storageContext
